2e7e9a9cf29b2ac6e95c74cb8995b9ae6a0e95c6
Sofy <sofy@olefir.com>
2019-10-16 22:20:46 +0300

    make name & write in file

---
+++ sources/make_name.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   make_name.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: solefir <solefir@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/16 13:01:13 by mtrotsen          #+#    #+#             */
+/*   Updated: 2019/10/16 22:19:15 by solefir          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/asm.h"
+
+char	*make_name(char *name_champ)
+{
+	char	*name;
+	int		i;
+	int		c;
+
+	i = 0;
+	c = 0;
+	while (name_champ[i] != '\0')
+		i++;
+	while (name_champ[i] != '.')
+		i--;
+	if (name_champ[(i + 1)] != 's')
+		return ("false");
+	name = malloc(sizeof(char) * i + COR_SIZE);
+	while (c < i)
+	{
+		name[c] = name_champ[c];
+		c++;
+	}
+	i = 0;
+	while (i < COR_SIZE)
+	{
+		name[c] = COR[i];
+		c++;
+		i++;
+	}
+	return (name);
+}
---
+++ sources/write_in_file.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   write_in_file.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: solefir <solefir@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/16 22:18:08 by solefir           #+#    #+#             */
+/*   Updated: 2019/10/16 22:18:53 by solefir          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/asm.h"
+
+void	write_uints(int fd, unsigned int out, _Bool size)
+{
+	int		j;
+	char	*to_out;
+	char	*now;
+
+	to_out = malloc(sizeof(char) * ((size ? 2 : 4)));
+	now = (char *)(&out);
+	now += ((size) ? 1 : 3);
+	j = 0;
+	while (j++ < ((size) ? 2 : 4))
+	{
+		to_out[j - 1] = *now;
+		now--;
+	}
+	write(fd, to_out, (size) ? 2 : 4);
+	free(to_out);
+}
+
+void	write_in_file(char *prog_name, t_holder *holder)
+{
+	int		fd;
+
+	fd = open(prog_name, O_WRONLY | O_CREAT);
+	//printf("%d\n", holder->bytes_count);
+	write_uints(fd, holder->header->magic, 0);
+	write(fd, holder->header->prog_name, PROG_NAME_LENGTH);
+	write(fd, "\0\0\0\0", 4);
+	write_uints(fd, holder->bytes_count, 0);
+	write(fd, holder->header->comment, COMMENT_LENGTH);
+	write(fd, "\0\0\0\0", 4);
+	printf("%s\n", holder->code->code);
+	// printf("HERE\n");
+	// while (holder->code->next != NULL)
+	// {
+	// 	printf("WHAT\n");
+	// 	write(fd, holder->code->code, holder->code->size);
+	// 	printf("KEK\n");
+	// 	holder->code = holder->code->next;
+	// 	printf("LOL\n");
+	// }
+	// printf("HERE\n");
+	// write(fd, holder->code->code, holder->code->size);
+	// printf("HERE\n");
+}
